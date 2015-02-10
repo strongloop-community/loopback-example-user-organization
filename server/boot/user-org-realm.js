a265b3c4f3e209d8ac70d7faa366f28734e16fbf
Raymond Feng <raymond@strongloop.com>
2015-02-10 13:52:53 -0800

    Clean up code

--- server/boot/user-org-realm.js
+++ server/boot/user-org-realm.js
@@ -2,8 +2,10 @@ module.exports = function(app) {
   var OrgUser = app.models.OrgUser;
   var Org = app.models.Org;
 
+  // Create an organization named as 'myRealm'
   Org.create({name: 'myRealm'}, function(err, org) {
     console.log(err, org);
+    // Create an user within the organization
     org.orgUsers.create(
       {
         username: 'john',
@@ -14,6 +16,8 @@ module.exports = function(app) {
       },
       function(err, user) {
         console.log(err, user);
+
+        // Now try to login with realm
         OrgUser.login({username: 'myRealm:john', password: '123'},
           function(err, result) {
             console.log(err, result);
